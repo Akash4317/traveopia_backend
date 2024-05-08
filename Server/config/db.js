faaba3c5da589e2b62d5ad1051a02b366a0b2faa
Akash Yadav <150064374+Akash4317@users.noreply.github.com>
2024-05-09 01:09:40 +0530

    backend

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+console.log("mongoURL:", process.env.mongoURL);
 const connection = mongoose.connect(process.env.mongoURL);
 
 module.exports = connection;
